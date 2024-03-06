b275dc82badac400249c7b01423db16118b7c740
asanchezmi <asanchezmi@ia.com.mx>
2024-03-05 21:17:16 -0600

    Principal

---
+++ AdlumenMVC.WebUI/Controllers/RolesController.cs
@@ -0,0 +1,240 @@
+﻿using AdlumenMVC.WebUI.Infraestructure;
+using AdlumenMVC.WebUI.Infrastructure;
+using AdlumenMVC.WebUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AdlumenMVC.WebUI.Controllers
+{
+
+
+    //[Authorize(Roles="Admin")]
+    [RoutePrefix("api/roles")]
+    public class RolesController : BaseApiController
+    {
+
+        [ClaimsAuthorization(Modulo = "Roles", ActionName = "Lectura")]
+        [Route("{id:guid}", Name = "GetRoleById")]
+        public async Task<IHttpActionResult> GetRole(string Id)
+        {
+            var role = await this.AppRoleManager.FindByIdAsync(Id);
+
+            if (role != null)
+            {
+                return Ok(TheModelFactory.Create(role));
+            }
+
+            return NotFound();
+
+        }
+
+        [ClaimsAuthorization(Modulo = "Roles", ActionName = "Lectura")]
+        [Route("roles")]
+        public IHttpActionResult GetRoles()
+        {
+            //Only SuperAdmin or Admin can delete users (Later when implement roles)
+            var identity = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            return Ok(this.AppRoleManager.Roles.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+
+        [ClaimsAuthorization(Modulo = "Roles", ActionName = "Escritura")]
+        [Route("create")]
+        public async Task<IHttpActionResult> Create(CreateRoleBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = new IdentityRole { Name = model.Name };
+            
+            var result = await this.AppRoleManager.CreateAsync(role);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            try
+            {
+                AddDefaultActions(role);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("No se agregaron los permisos de Usuarios. Agreguelos manualmente.");
+            }
+
+            Uri locationHeader = new Uri(Url.Link("GetRoleById", new { id = role.Id }));
+
+            return Created(locationHeader, TheModelFactory.Create(role));
+        }
+
+        [ClaimsAuthorization(Modulo = "Roles", ActionName = "Escritura")]
+        [Route("deleterole/{id:guid}")]
+        [HttpPost]
+        public async Task<IHttpActionResult> DeleteRole(string id)
+        {
+            var role = await this.AppRoleManager.FindByIdAsync(id);
+
+            if (role != null)
+            {
+                var result = await this.AppRoleManager.DeleteAsync(role);
+
+                if (!result.Succeeded)
+                {
+                    return GetErrorResult(result);
+                }
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [ClaimsAuthorization(Modulo = "Roles", ActionName = "Escritura")]
+        [Route("ManageUsersInRole")]
+        public async Task<IHttpActionResult> ManageUsersInRole(UsersInRoleModel model)
+        {
+            var role = await this.AppRoleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ModelState.AddModelError("", "Role does not exist");
+                return BadRequest(ModelState);
+            }
+
+            foreach (string user in model.EnrolledUsers)
+            {
+                var appUser = await this.AppUserManager.FindByIdAsync(user);
+
+                if (appUser == null)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} does not exists", user));
+                    continue;
+                }
+
+                if (!this.AppUserManager.IsInRole(user, role.Name))
+                {
+                    IdentityResult result = await this.AppUserManager.AddToRoleAsync(user, role.Name);
+
+                    if (!result.Succeeded)
+                    {
+                        ModelState.AddModelError("", String.Format("User: {0} could not be added to role", user));
+                    }
+
+                }
+            }
+
+            foreach (string user in model.RemovedUsers)
+            {
+                var appUser = await this.AppUserManager.FindByIdAsync(user);
+
+                if (appUser == null)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} does not exists", user));
+                    continue;
+                }
+
+                IdentityResult result = await this.AppUserManager.RemoveFromRoleAsync(user, role.Name);
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} could not be removed from role", user));
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("fixroles")]
+        public IHttpActionResult FixRoles1()
+        {
+            try
+            {
+                using (var db = new ApplicationDbContext())
+                {
+                    var roles = db.Roles.ToList();
+                    var actions = db.AccionesRoles.ToList();
+                    var action2 = db.Acciones.FirstOrDefault(x => x.Nombre == "Lectura" && x.ModuloId == 4);
+                    var action1 = db.Acciones.FirstOrDefault(x => x.Nombre == "GetUserById" && x.ModuloId == 4);
+
+                    if (action1 == null || action2 == null)
+                    {
+                        return Ok("Las acciones predefinidas de Usuario no existen.");
+                    }
+
+                    foreach (var role in roles)
+                    {
+                        var permissionExists = actions.Any(x =>
+                            x.RoleId == role.Id &&
+                            x.AccionesId == action1.AccionesId);
+
+                        if (!permissionExists)
+                        {
+                            db.AccionesRoles.Add(new AccionesRole
+                            {
+                                RoleId = role.Id,
+                                AccionesId = action1.AccionesId
+                            });
+                        }
+
+                        permissionExists = actions.Any(x =>
+                            x.RoleId == role.Id &&
+                            x.AccionesId == action2.AccionesId);
+
+                        if (!permissionExists)
+                        {
+                            db.AccionesRoles.Add(new AccionesRole
+                            {
+                                RoleId = role.Id,
+                                AccionesId = action2.AccionesId
+                            });
+                        }
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok("Error. Intente de nuevo.<br><br>" + ex.Message);
+            }
+
+            return Ok("Los roles han sido corregidos.");
+        }
+
+        private void AddDefaultActions(IdentityRole role)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var action2 = db.Acciones.FirstOrDefault(x => x.Nombre == "Lectura" && x.ModuloId == 4);
+                var action1 = db.Acciones.FirstOrDefault(x => x.Nombre == "GetUserById" && x.ModuloId == 4);
+
+                db.AccionesRoles.Add(new AccionesRole
+                {
+                    RoleId = role.Id,
+                    AccionesId = action1.AccionesId
+                });
+
+                db.AccionesRoles.Add(new AccionesRole
+                {
+                    RoleId = role.Id,
+                    AccionesId = action2.AccionesId
+                });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ AdlumenMVC.WebUI/app/controllers/mensajesCtrl.js
@@ -0,0 +1,272 @@
+﻿'use strict';
+adlumenApp.controller('mensajesCtrl',
+    [
+        '$scope', 'mensajeAPI','usuarioAPI','translationService','languageService', '$sce', 'Restangular', '$http', '$q', '$location', '$window',
+        function ($scope, mensajeAPI, usuarioAPI, translationService, languageService, $sce, Restangular, $http, $q, $location, $window) {
+
+            mensajeAPI($scope);
+
+            usuarioAPI($scope);
+
+            $scope.message = {};
+
+            $scope.views = { showInbox: true, showSent: false, showDraft: false, showCompose: false, showDeleted: false };
+
+            $scope.tinymceOptions = {
+                menubar: false,
+                statusbar: false,
+                plugins: 'lists',
+                language: 'es',
+                toolbar1: 'insertfile | styleselect | bold italic underline | bullist numlist outdent indent',
+                style_formats: [
+                    { title: 'Encabezado 1', block: 'h1' },
+                    { title: 'Encabezado 2', block: 'h2' },
+                    { title: 'Encabezado 3', block: 'h3' },
+                    { title: 'Encabezado 4', block: 'h4' },
+                    { title: 'Encabezado 5', block: 'h5' },
+                    { title: 'Encabezado 6', block: 'h6' }
+                ]
+            }
+
+            $scope.setMessageView = function (id, lastView, message) {
+                _.each($scope.views, function (value, key) {
+                    $scope.views[key] = false;
+                });
+                $scope.attachmentName = null;
+                $scope.attachmentKey = null;
+                switch (id) {
+                    case 1:
+                        $scope.views.showInbox = true;
+                        break;
+                    case 2:
+                        $scope.views.showSent = true;
+                        break;
+                    case 3:
+                        $scope.views.showDraft = true;
+                        break;
+                    case 4:
+                        $scope.views.showCompose = true;
+                        break;
+                    case 5:
+                        $scope.views.showDeleted = true;
+                        break;
+                    case 6:
+                        $scope.views.showReadMessage = true;
+                        if (!_.isUndefined(lastView) && !_.isUndefined(message)) {
+                            $scope.lastView = lastView;
+                            $scope.selectedMessage = message;
+                            $scope.checkForAttachment(message);
+                        }
+                        break;
+                }
+            };
+
+            $scope.checkForAttachment = function (message) {
+                $http.get('api/mensajes/GetAttachmentUrl?idMessage=' + message.idMensaje).then(function (response) {
+                    $scope.attachmentName = response.data.name;
+                    $scope.attachmentKey = response.data.key;
+                });
+            }
+
+            $scope.validateAttachment = function (element) {
+                if (element.files[0].size > (32 * 1024 * 1024)) {
+                    alert("Tamaño máximo: 32 MB");
+                    element.value = "";
+                }
+                else {
+                    var fileName = element.files[0].name;
+                    $('#custom-file-label').html(fileName);
+                }
+            }
+
+            $scope.$watchCollection('[bdUser, mensajes]', function (newValue, oldValue) {
+
+                if (!_.isUndefined(newValue[1]) && !_.isUndefined(newValue[0])) {
+                    $scope.user = newValue[0];
+                    $scope.messages = newValue[1];
+                    $scope.inbox = _.filter($scope.messages, function (message) {
+                        return message.idEstado != 3 && message.idUsuarioDestinatario === $scope.user.idLocal;
+                    });
+
+                    $scope.newMessages = (_.where($scope.inbox, { idEstado: 1 })).length;
+
+                    $scope.deleted = _.filter($scope.messages, function (message) {
+                        return message.idEstado === 3 && (message.idUsuarioDestinatario === $scope.user.idLocal || message.idUsuarioRemitente === $scope.user.idLocal);
+                    });
+
+                    $scope.draft = _.filter($scope.messages, function (message) {
+                        return message.idEstado === 4;
+                    });
+
+                    $scope.sent = _.filter($scope.messages, function (message) {
+                        return message.idUsuarioRemitente === $scope.user.idLocal && message.idEstado != 3;
+                    });
+
+                }
+
+            });
+
+            $scope.dateAgo = function (dateEval) {
+
+                var value;
+
+                var datediff = (new Date()).getTime() - dateEval.getTime();
+
+                var days = datediff / (1000 * 60 * 60 * 24);
+
+                var years = datediff / (1000 * 365 * 60 * 60 * 24);
+
+                var hours = datediff / (1000 * 60 * 60);
+
+                var minutes = datediff / (1000 * 60);
+
+                var months = datediff / (1000 * 60 * 60 * 24 * 30);
+
+
+                if (days.toFixed() >= 1 || days.toFixed() <= 30) {
+                    value = days.toFixed() + ' ' + $scope.translation.DIAS;
+                } else if (month.toFixed() >= 1 || days.toFixed() <= 12) {
+                    value = days.toFixed() + ' ' + $scope.translation.MESES;
+                } else if (years.toFixed() >= 1 || days.toFixed() <= 12) {
+                    value = days.toFixed() + ' ' + $scope.translation.MESES;
+                } else if (month.toFixed() >= 1 || days.toFixed() <= 12) {
+                    value = days.toFixed() + ' ' + $scope.translation.MESES;
+                } else if (month.toFixed() >= 1 || days.toFixed() <= 12) {
+                    value = days.toFixed() + ' ' + $scope.translation.MESES;
+                }
+
+                return value;
+            };
+
+            $scope.showAlert = false;
+
+
+             var addAlert = function (varType, varMsg) {
+                $scope.alerts.push({ type: varType, msg: varMsg });
+                $scope.showAlert = true;
+            }
+
+            $scope.inputMensaje = {
+                txtCorreo: "",
+                txtAsunto: "",
+                txtMensaje: "",
+                txtPrioridad: "",
+            }
+
+            $scope.resetTexts = function () {
+                $scope.inputMensaje.txtAsunto = "";
+                $scope.inputMensaje.txtMensaje = "";
+                
+                $scope.newMensaje = false;
+            };
+
+            $scope.addMensaje = function () {
+                $scope.message.idUsuarioRemitente = $scope.user.idLocal;
+                $scope.message.prioridad = false;
+                $scope.message.idEstado = 1;
+                $scope.alerts = [];
+                var data = new FormData();
+                data.append('idUsuarioRemitente', $scope.user.idLocal);
+                data.append('prioridad', false);
+                data.append('idEstado', 1);
+                data.append('idUsuarioDestinatario', $scope.message.idUsuarioDestinatario);
+                data.append('asunto', $scope.message.asunto);
+                data.append('mensaje', $scope.message.mensaje);
+                
+                if ($('#attachment')[0].files.length > 0) {
+                    $scope.message.attachment = $('#attachment')[0].files[0];
+                    data.append('attachment', $('#attachment')[0].files[0])
+                }
+                var defer = $q.defer();
+                $http.post("/api/Mensajes/AddMensaje", data,
+                {
+                    withCredentials: true,
+                    headers: { 'Content-Type': undefined },
+                    transformRequest: angular.identity
+                }).then(
+                    function (d) {
+                        addAlert('success', $scope.translation.MENSAJE_BITACORA_GUARDADA);
+                        mensajeAPI($scope);
+                        $scope.setMessageView(1);
+                    },
+                    function () {
+                        addAlert('danger', 'Error');
+                    }
+                );
+                //Restangular.one('api/mensajes')
+                //    .withHttpConfig({ transformRequest: angular.identity })
+                //    .customPOST(data, '', undefined, { 'Content-Type': undefined })
+
+                //$scope.mensajes.post($scope.message).then(function () {
+                //    addAlert('success', $scope.translation.MENSAJE_BITACORA_GUARDADA);
+                //    mensajeAPI($scope);
+                //    setMessageView(1);
+                //}, function (error) {
+                //    addAlert('danger', error.data.exceptionMessage);
+                //});
+            }
+
+            //Sirve para ver los mensajes Recibicos, Enviados
+            $scope.selectMensajesRecibidos = function (mensaje) {
+                $scope.newMensaje = mensaje;
+                $scope.selected = true;
+                $scope.Recibidos = false;
+                $scope.leer = true;
+
+            }
+            $scope.selectMensajesEnviados = function (mensaje) {
+                $scope.newMensaje = mensaje;
+                $scope.selected = true;
+                $scope.Enviados = false;
+                $scope.leer = true;
+
+            }
+            $scope.selectMensajesElimados = function (mensaje) {
+                $scope.newMensaje = mensaje;
+                $scope.selected = true;
+                $scope.Eliminados = false;
+                $scope.leer = true;
+
+            }
+           
+            
+        //Sirve par cambiar color de los que no se ha leido
+            $scope.set_color = function (mensaje) {
+                if (mensaje.prioridad == true ) {
+                    return {
+                                             
+                        'background-color': "#09A225",
+                        color: 'white'
+                    }
+                if (mensaje.idEstado == 1) {
+                    return {
+                        'background-color': "#076F91",
+                        color: 'black'
+                    }
+
+                }
+               
+               }
+                
+                
+            }
+///esta
+            $scope.edit = function (mensaje, numero) {
+                var noMensaje = mensaje.idMensaje;
+                var numero2 = numero;
+                $scope.newMensaje.put({ idMensaje: noMensaje, estado: numero2 }).then(function () {
+
+                    mensajeAPI($scope);
+                  
+                }, function () {
+                }
+
+                );
+            }
+
+            $scope.displayHtml = function () {
+                return $sce.trustAsHtml($scope.selectedMessage.mensaje);
+            };
+        }
+    ]
+);
